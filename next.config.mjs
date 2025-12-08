56f1f9529fd22c9aecd470e8ad99aa2464b71c7f
Ivtsan <dan.ivtsan@gmail.com>
2025-12-07 19:22:19 -0500

    next js fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-  // Reduce file watching overhead
-  webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
-      // Optimize for faster rebuilds
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-        ignored: /node_modules/,
-      }
-    }
-    return config
-  },
+  turbopack: {},
 }
 
 export default nextConfig
